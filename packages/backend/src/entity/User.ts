ac4538ca83ea8129834361716aa5da4bea6e486a
Keven Leone <keven.santos.sz@gmail.com>
2021-02-26 14:25:56 -0300

    Use MainEntity instead BaseEntity

--- packages/backend/src/entity/User.ts
+++ packages/backend/src/entity/User.ts
@@ -1,24 +1,13 @@
-import { Field, ID, ObjectType } from 'type-graphql';
-import {
-  BaseEntity,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  OneToOne,
-  PrimaryColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Field, ObjectType } from 'type-graphql';
+import { Entity, ManyToOne, OneToOne } from 'typeorm';
 
 import { Github } from './Github';
+import { MainEntity } from './MainEntity';
 import { Team } from './Team';
 
 @ObjectType()
 @Entity()
-export class User extends BaseEntity {
-  @Field(() => ID)
-  @PrimaryColumn({ generated: 'uuid' })
-  id: string;
-
+export class User extends MainEntity {
   @Field(() => Github)
   @OneToOne(() => Github, (github) => github.user)
   github: Github;
@@ -26,12 +15,4 @@ export class User extends BaseEntity {
   @Field(() => Team, { nullable: true })
   @ManyToOne(() => Team, (team) => team.users)
   team: Team;
-
-  @Field()
-  @CreateDateColumn()
-  created_at: Date;
-
-  @Field()
-  @UpdateDateColumn()
-  updated_at: Date;
 }
