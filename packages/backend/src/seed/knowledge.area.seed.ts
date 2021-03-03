6d1eef7174cd0a4ad9e2453b4f3271d3bc1e33a5
Keven Leone <keven.santos.sz@gmail.com>
2021-03-03 13:53:57 -0300

    Creation of Seed to Office and Knowledge -> Area, Matriz and Skill

---
+++ packages/backend/src/seed/knowledge.area.seed.ts
@@ -0,0 +1,70 @@
+export default [
+  {
+    name: 'Backend',
+    skills: [
+      'Java 8',
+      'Spark',
+      'Python',
+      'ElasticSearch',
+      'OSGi',
+      'Spring Boot',
+      'Gradle',
+      'Apache Ant',
+      'C++',
+    ],
+  },
+  {
+    name: 'Common Skills',
+    skills: ['Github', 'GraphQL', 'Docker', 'Unit Test', 'Integration Test'],
+  },
+  {
+    name: 'Frontend',
+    skills: [
+      'React',
+      'Angular',
+      'JavaScript',
+      'Typescript',
+      'CSS / SCSS',
+      'HTML5',
+      'Flutter',
+      'Jquery',
+    ],
+  },
+  {
+    name: 'Quality Assurance',
+    skills: ['Poshi', 'TestRay', 'Cumcumber', 'Cypress', 'Selenium'],
+  },
+  {
+    name: 'Project Management',
+    skills: [
+      'Jira',
+      'Confluence',
+      'Risk Management',
+      'Scrum',
+      'Kanban',
+      'Conflict Management',
+      'Problem Solving',
+    ],
+  },
+  {
+    name: 'Communication',
+    skills: ['English Skills', 'Speech', 'Feedback', 'Essay', 'Reporting'],
+  },
+  {
+    name: 'Product Design',
+    skills: [
+      'Figma',
+      'Usability',
+      'Interactions',
+      'Testing',
+      'Prototyping',
+      'Business',
+      'Principle',
+      'Balsamiq',
+    ],
+  },
+  {
+    name: 'Others Skills',
+    skills: ['Sing', 'Dance'],
+  },
+];
---
+++ packages/backend/src/seed/knowledge.matriz.seed.ts
@@ -0,0 +1,37 @@
+export default [
+  {
+    description:
+      'It has no knowledge and experience. It is a beginner in the theme.',
+    name: 'Beginner',
+  },
+  {
+    description:
+      'Has a little knowledge and little skill. It is taking its first steps in the theme and is learning.',
+    name: 'Apprentice',
+  },
+  {
+    description:
+      'Has a good knowledge of the subject. You are practicing without major problems, getting good results.',
+    name: 'Practitioner',
+  },
+  {
+    description:
+      'Has a good knowledge and skill on the subject. It also takes the lead in solving problems.',
+    name: 'Professional',
+  },
+  {
+    description:
+      'Master the theme. It is an expert. It stands out for teaching other people. It is a reference for the team.',
+    name: 'Teacher',
+  },
+  {
+    description:
+      'He/she can combine knowledge to further improve his/her work, and is a natural leader on this topic.',
+    name: 'Leader',
+  },
+  {
+    description:
+      'It creates new knowledge on the subject, totally dominates its execution and is an inspiration to the team.',
+    name: 'Master',
+  },
+];
