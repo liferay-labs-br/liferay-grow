d3af42a1fee68c919b753a99f3b97e9564ee50fe
Keven Leone <keven.santos.sz@gmail.com>
2021-03-03 18:13:35 -0300

    Create Role Seed and Migration #13

---
+++ packages/backend/src/seed/role.seed.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'Backend',
+  },
+  {
+    name: 'Frontend',
+  },
+  {
+    name: 'Quality Assurance',
+  },
+  {
+    name: 'Project Management',
+  },
+  {
+    name: 'Product Owner',
+  },
+  {
+    name: 'Product Design',
+  },
+];
