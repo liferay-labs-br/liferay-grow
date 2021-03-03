3a52b897d6a708054fad415186a72eeed5920af4
Keven Leone <keven.santos.sz@gmail.com>
2021-03-03 18:03:52 -0300

    Create Role Entity and resolver #11

---
+++ packages/backend/src/resolvers/role/role.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver } from 'type-graphql';
+
+import { Role } from '../../entity/Role';
+import { createBaseResolver } from '../../utils/createBaseResolver';
+import Inputs from './Inputs';
+
+const BaseResolver = createBaseResolver('Role', Role, Inputs);
+
+@Resolver(Role)
+export class RoleResolver extends BaseResolver {}
