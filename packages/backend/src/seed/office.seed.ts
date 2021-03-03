79e2a7b76623ff429649e3cb48596956abeb6a55
Keven Leone <keven.santos.sz@gmail.com>
2021-03-03 13:53:57 -0300

    Creation of Seed to Office and Knowledge -> Area, Matriz and Skill

---
+++ packages/backend/src/seed/office.seed.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    address: 'R. dos Arcos, 160 - Poço da Panela, Recife - PE, 52061-180',
+    city: 'Recife',
+    country: 'Brazil',
+    name: 'Poço da Panela',
+    state: 'Pernambuco',
+  },
+  {
+    address: 'Paseo de la Castellana, 280, 28046 Madrid, Espanha',
+    city: 'Madrid',
+    country: 'Spain',
+    name: 'Madri',
+    state: 'Madrid',
+  },
+  {
+    address: '1400 Montefino Ave, Diamond Bar, CA 91765, Estados Unidos',
+    city: 'Diamond Bar',
+    country: 'United States',
+    name: 'Diamond Bar',
+    state: 'California',
+  },
+  {
+    address: 'Budapest, Madarász Viktor utca 47 a-b, 1138 Hungria',
+    city: 'Budapeste',
+    country: 'Hungary',
+    name: 'Hungary',
+    state: 'Budapeste',
+  },
+];
