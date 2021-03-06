311933d7011d80687b33405863235ea752f41c34
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-03-06 18:13:51 -0300

    Make flexible the WelcomeContent component to add custom buttons at footer

--- packages/frontend/src/pages/welcome/get-started.tsx
+++ packages/frontend/src/pages/welcome/get-started.tsx
@@ -1,50 +1,73 @@
+import ClayButton from '@clayui/button';
 import ClayForm, { ClaySelect } from '@clayui/form';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import WelcomeContent from '../../components/welcome/WelcomeContent';
 import WelcomeTmpl from '../../components/welcome/WelcomeTmpl';
 import WrappedSafeComponent from '../../components/WrappedSafeComponent';
 import { getStarted } from '../../graphql/queries';
 import withAuth from '../../hocs/withAuth';
+import useLang from '../../hooks/useLang';
+
+const GetStartedBody: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
+  return (
+    <WrappedSafeComponent query={getStarted}>
+      {({ careerDepartaments, offices }) => {
+        return (
+          <ClayForm>
+            <ClayForm.Group>
+              <label htmlFor="office">Office</label>
+              <ClaySelect>
+                {offices.map((office) => (
+                  <optgroup key={office.id} label={office.name}>
+                    {office.teams.map((team) => (
+                      <option key={team.id} value={team.id}>
+                        {team.name}
+                      </option>
+                    ))}
+                  </optgroup>
+                ))}
+              </ClaySelect>
+            </ClayForm.Group>
+            <ClayForm.Group>
+              <label htmlFor="job-title">Job Title</label>
+              <ClaySelect>
+                {careerDepartaments.map((departament) => (
+                  <optgroup key={departament.id} label={departament.name}>
+                    {departament.careers.map((career) => (
+                      <option key={career.id} value={career.id}>
+                        {career.name}
+                      </option>
+                    ))}
+                  </optgroup>
+                ))}
+              </ClaySelect>
+            </ClayForm.Group>
+          </ClayForm>
+        );
+      }}
+    </WrappedSafeComponent>
+  );
+};
 
 const GetStarted: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
+  const i18n = useLang();
+  const router = useRouter();
+
   return (
     <WelcomeTmpl>
-      <WrappedSafeComponent query={getStarted}>
-        {({ careerDepartaments, offices }) => {
-          return (
-            <ClayForm>
-              <ClayForm.Group>
-                <label htmlFor="office">Office</label>
-                <ClaySelect>
-                  {offices.map((office) => (
-                    <optgroup key={office.id} label={office.name}>
-                      {office.teams.map((team) => (
-                        <option key={team.id} value={team.id}>
-                          {team.name}
-                        </option>
-                      ))}
-                    </optgroup>
-                  ))}
-                </ClaySelect>
-              </ClayForm.Group>
-              <ClayForm.Group>
-                <label htmlFor="job-title">Job Title</label>
-                <ClaySelect>
-                  {careerDepartaments.map((departament) => (
-                    <optgroup key={departament.id} label={departament.name}>
-                      {departament.careers.map((career) => (
-                        <option key={career.id} value={career.id}>
-                          {career.name}
-                        </option>
-                      ))}
-                    </optgroup>
-                  ))}
-                </ClaySelect>
-              </ClayForm.Group>
-            </ClayForm>
-          );
-        }}
-      </WrappedSafeComponent>
+      <WelcomeContent>
+        <WelcomeContent.Title>{i18n.get('get-started')}</WelcomeContent.Title>
+        <WelcomeContent.Body>
+          <GetStartedBody />
+        </WelcomeContent.Body>
+        <WelcomeContent.Footer>
+          <ClayButton onClick={() => router.push('skills-details')}>
+            {i18n.get('next')}
+          </ClayButton>
+        </WelcomeContent.Footer>
+      </WelcomeContent>
     </WelcomeTmpl>
   );
 };
--- packages/frontend/src/pages/welcome/skills-details.tsx
+++ packages/frontend/src/pages/welcome/skills-details.tsx
@@ -1,10 +1,39 @@
+import ClayButton from '@clayui/button';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import WelcomeContent from '../../components/welcome/WelcomeContent';
 import WelcomeTmpl from '../../components/welcome/WelcomeTmpl';
 import withAuth from '../../hocs/withAuth';
+import useLang from '../../hooks/useLang';
 
-const SkillsDetails: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
-  return <WelcomeTmpl>{'skills details content...'}</WelcomeTmpl>;
+const KnowledgeGaps: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
+  const i18n = useLang();
+  const router = useRouter();
+
+  return (
+    <WelcomeTmpl>
+      <WelcomeContent>
+        <WelcomeContent.Title>
+          {i18n.get('skills-details')}
+        </WelcomeContent.Title>
+        <WelcomeContent.Body>{'skills details content...'}</WelcomeContent.Body>
+        <WelcomeContent.Footer>
+          <ClayButton
+            displayType="secondary"
+            className="mr-2"
+            onClick={() => router.push('get-started')}
+          >
+            {i18n.get('prev')}
+          </ClayButton>
+
+          <ClayButton onClick={() => router.push('knowledge-gaps')}>
+            {i18n.get('next')}
+          </ClayButton>
+        </WelcomeContent.Footer>
+      </WelcomeContent>
+    </WelcomeTmpl>
+  );
 };
 
-export default withAuth(SkillsDetails);
+export default withAuth(KnowledgeGaps);
