eff5fc8dfe818e2a77cd2e16910aaa884375e3d4
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-03-06 17:08:48 -0300

    Go to welcome page after render dashboard

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import SEO from '../components/meta';
 import withAuth from '../hocs/withAuth';
@@ -9,23 +9,11 @@ const Index = () => {
   const i18n = useLang();
   const router = useRouter();
 
-  return (
-    <div
-      style={{
-        alignContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        display: 'flex',
-        height: '100%',
-        justifyContent: 'center',
-      }}
-    >
-      <SEO title={i18n.sub('app-title-x', 'Dashboard')} />
-      <button type="button" onClick={() => router.push('/welcome')}>
-        Some Dashboard
-      </button>
-    </div>
-  );
+  useEffect(() => {
+    router.push('/welcome');
+  }, []);
+
+  return <SEO title={i18n.sub('app-title-x', 'Dashboard')} />;
 };
 
 export default withAuth(Index);
