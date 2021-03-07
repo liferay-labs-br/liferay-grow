d929215cd8c0de21e871fb24221a8c310a18fb3d
Keven Leone <keven.santos.sz@gmail.com>
2021-03-06 23:02:14 -0300

    Fixes: #30 - Add Seed to store Teams and Office

--- packages/backend/src/migration/1614781968294-SeedOffice.ts
+++ packages/backend/src/migration/1614781968294-SeedOffice.ts
@@ -4,7 +4,21 @@ import OfficeSeed from '../seed/office.seed';
 
 export class SeedOffice1614781968294 implements MigrationInterface {
   public async up(): Promise<void> {
-    await getRepository('office').save(OfficeSeed);
+    const teamPromises = [];
+    for (const officeSeed of OfficeSeed) {
+      const { teams, ...officeData } = officeSeed;
+      const office = await getRepository('office').save(officeData);
+
+      for (const team of teams) {
+        teamPromises.push(
+          getRepository('team').save({
+            ...team,
+            office,
+          }),
+        );
+      }
+    }
+    await Promise.all(teamPromises);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
