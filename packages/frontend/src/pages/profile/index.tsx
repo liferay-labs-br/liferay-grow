a64d916b9dd250e1ff268633bebfcc89424e96fc
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-03-11 17:36:10 -0300

    #44 ProfilePanel refactored to composition

--- packages/frontend/src/pages/profile/index.tsx
+++ packages/frontend/src/pages/profile/index.tsx
@@ -1,25 +1,91 @@
+import ClayProgressBar from '@clayui/progress-bar';
 import React from 'react';
 
 import SEO from '../../components/meta';
 import Profile, { ProfileWrapper } from '../../components/profile';
-import WrappedSafeComponent from '../../components/WrappedSafeComponent';
-import { getMe } from '../../graphql/queries';
+import ProfilePanel from '../../components/profile/ProfilePanel';
 import withAuth from '../../hocs/withAuth';
 import useLang from '../../hooks/useLang';
 
 const Index = () => {
   const i18n = useLang();
 
+  const skillDetails = [
+    {
+      id: '1',
+      knowledgeMatriz: {
+        name: 'Beginner',
+      },
+      knowledgeSkill: {
+        name: 'Java',
+      },
+    },
+    {
+      id: '2',
+      knowledgeMatriz: {
+        name: 'Professional',
+      },
+      knowledgeSkill: {
+        name: 'Prototyping',
+      },
+    },
+    {
+      id: '3',
+      knowledgeMatriz: {
+        name: 'Beginner',
+      },
+      knowledgeSkill: {
+        name: 'Typescript',
+      },
+    },
+  ];
+
+  const gapsDetails = [
+    {
+      id: '1',
+      knowledgeSkill: {
+        name: 'Java',
+      },
+    },
+    {
+      id: '2',
+      knowledgeSkill: {
+        name: 'Prototyping',
+      },
+    },
+    {
+      id: '3',
+      knowledgeSkill: {
+        name: 'Typescript',
+      },
+    },
+  ];
+
   return (
     <Profile>
-      <WrappedSafeComponent query={getMe}>
-        {({ me }) => (
-          <>
-            <SEO title={i18n.sub('app-title-x', 'Profile')} />
-            <ProfileWrapper me={me} />
-          </>
-        )}
-      </WrappedSafeComponent>
+      <SEO title={i18n.sub('app-title-x', 'Profile')} />
+      <ProfileWrapper>
+        <ProfilePanel title={i18n.get('skill-details')}>
+          {skillDetails.map(
+            ({ id, knowledgeMatriz, knowledgeSkill: { name: skillName } }) => (
+              <ProfilePanel.Item key={id}>
+                <ProfilePanel.Title>{skillName}</ProfilePanel.Title>
+                <ProfilePanel.Body>
+                  <span>{knowledgeMatriz.name}</span>
+                </ProfilePanel.Body>
+                <ClayProgressBar value={20} />
+              </ProfilePanel.Item>
+            ),
+          )}
+        </ProfilePanel>
+        <ProfilePanel title={i18n.get('skill-details')}>
+          {gapsDetails.map(({ id, knowledgeSkill: { name: skillName } }) => (
+            <ProfilePanel.Item key={id}>
+              <ProfilePanel.Title>{skillName}</ProfilePanel.Title>
+            </ProfilePanel.Item>
+          ))}
+        </ProfilePanel>
+      </ProfileWrapper>
     </Profile>
   );
 };
