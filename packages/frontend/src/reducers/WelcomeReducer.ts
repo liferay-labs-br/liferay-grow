dbbbd903e21eb8fb08a8b1321ee03109babbc6c6
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-03-11 02:23:21 -0300

    #43 Create WelcomeReducer to manage data during welcome steps

---
+++ packages/frontend/src/reducers/WelcomeReducer.ts
@@ -0,0 +1,60 @@
+import { ActionMap, ActionsPayload, Types } from '../types';
+
+type Steps = {
+  checked: boolean;
+  value: string;
+}[];
+
+type Welcome = {
+  steps: Steps;
+};
+
+type WelcomeActions = ActionMap<ActionsPayload>[keyof ActionMap<ActionsPayload>];
+
+const STEPS: Steps = [
+  { checked: false, value: 'get-started' },
+  { checked: false, value: 'skills-details' },
+  { checked: false, value: 'knowledge-gaps' },
+];
+
+export const welcomeState = {
+  steps: STEPS,
+};
+
+export const welcomeReducer = (
+  state: {
+    steps: Steps;
+  },
+  action: WelcomeActions,
+): Welcome => {
+  switch (action.type) {
+    case Types.SET_GET_STARTED_DATA: {
+      return state;
+    }
+    case Types.SET_SKILLS_DATA: {
+      return state;
+    }
+    case Types.SET_KNOWLEDGE_GAPS_DATA: {
+      return state;
+    }
+    case Types.UPDATE_STEP: {
+      const { checked, value } = action.payload;
+
+      return {
+        ...state,
+        steps: state.steps.map((step) => {
+          if (step.value === value) {
+            return {
+              ...step,
+              checked,
+            };
+          }
+
+          return step;
+        }),
+      };
+    }
+    default:
+      return state;
+  }
+};
