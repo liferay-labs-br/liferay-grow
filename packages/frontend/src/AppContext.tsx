0d204a348d431f6bf771d58dcedfd8b32ff5f309
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-03-11 00:16:11 -0300

    #43 Create WelcomeReducer to manage data during welcome steps

--- packages/frontend/src/AppContext.tsx
+++ packages/frontend/src/AppContext.tsx
@@ -2,10 +2,12 @@ import { createContext, Dispatch } from 'react';
 
 import { portalReducer, portalState } from './reducers/PortalReducer';
 import { userReducer, userState } from './reducers/UserReducer';
+import { welcomeReducer, welcomeState } from './reducers/WelcomeReducer';
 
 const initialState = {
   portal: portalState,
   user: userState,
+  welcome: welcomeState,
 };
 
 const AppContext = createContext<{
@@ -22,6 +24,7 @@ const mainReducer = (
 ): InitialStateAppContextType => ({
   portal: portalReducer(portal, action),
   user: userReducer(user, action),
+  welcome: welcomeReducer(welcome, action),
 });
 
 export { mainReducer, initialState };
