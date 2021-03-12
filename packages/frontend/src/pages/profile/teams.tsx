d0efa111b0b8272a159377351d4342545c6dc2e0
Keven Leone <keven.santos.sz@gmail.com>
2021-03-12 15:59:57 -0300

    #35 Get User team from GraphQL

--- packages/frontend/src/pages/profile/teams.tsx
+++ packages/frontend/src/pages/profile/teams.tsx
@@ -1,39 +1,15 @@
 import React from 'react';
 
-import SEO from '../../components/meta';
-import Profile, { ProfileWrapper } from '../../components/profile';
-import ProfilePanel from '../../components/profile/ProfilePanel';
 import WrappedSafeComponent from '../../components/WrappedSafeComponent';
-import { getAllTeam } from '../../graphql/queries';
+import { getMe } from '../../graphql/queries';
 import withAuth from '../../hocs/withAuth';
-import useLang from '../../hooks/useLang';
+import { Me } from '../../types';
+import { TeamTemplate } from './_template';
 
-const Index = () => {
-  const i18n = useLang();
+const Teams = () => (
+  <WrappedSafeComponent query={getMe}>
+    {({ me }: { me: Me }) => <TeamTemplate me={me} />}
+  </WrappedSafeComponent>
+);
 
-  return (
-    <WrappedSafeComponent query={getAllTeam}>
-      {({ teams }) => (
-        <Profile>
-          <SEO title={i18n.sub('app-title-x', 'Profile')} />
-          <ProfileWrapper>
-            <ProfilePanel title={i18n.get('skill-details')}>
-              {teams.map(({ id, name }) => (
-                <ProfilePanel.Item key={id}>
-                  <ProfilePanel.Title className="title">
-                    {name}
-                  </ProfilePanel.Title>
-                  <ProfilePanel.Body>
-                    <span>21 Members</span>
-                  </ProfilePanel.Body>
-                </ProfilePanel.Item>
-              ))}
-            </ProfilePanel>
-          </ProfileWrapper>
-        </Profile>
-      )}
-    </WrappedSafeComponent>
-  );
-};
-
-export default withAuth(Index);
+export default withAuth(Teams);
