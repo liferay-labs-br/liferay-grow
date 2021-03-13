9dd1d0c8b260c2a3ad59332b37706f1b16ef5eba
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-03-13 20:14:39 -0300

    #59 created Header component

---
+++ packages/frontend/src/components/header/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const HeaderAvatar: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({
+  ...props
+}) => {
+  return (
+    <div>
+      <img className="header__avatar" {...props}></img>
+    </div>
+  );
+};
+
+const HeaderTitle: React.FC<React.HTMLAttributes<HTMLElement>> = ({
+  children,
+}) => {
+  return <h1 className="header__info--title">{children}</h1>;
+};
+
+const HeaderInfo: React.FC<React.HTMLAttributes<HTMLElement>> = ({
+  children,
+}) => {
+  return <div className="header__info">{children}</div>;
+};
+
+const Header: React.FC & {
+  Avatar: React.ElementType;
+  Info: React.ElementType;
+  Title: React.ElementType;
+} = ({ children }) => {
+  return <div className="header">{children}</div>;
+};
+
+Header.Info = HeaderInfo;
+Header.Avatar = HeaderAvatar;
+Header.Title = HeaderTitle;
+export default Header;
