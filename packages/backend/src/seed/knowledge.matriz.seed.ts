291ee633aff23f8068a9f5392cfca6f320613572
Keven Leone <keven.santos.sz@gmail.com>
2021-03-13 19:45:16 -0300

    #57 add matriz level in KnowledgeMatriz

--- packages/backend/src/seed/knowledge.matriz.seed.ts
+++ packages/backend/src/seed/knowledge.matriz.seed.ts
@@ -2,36 +2,43 @@ export default [
   {
     description:
       'It has no knowledge and experience. It is a beginner in the theme.',
+    matrizLevel: 1,
     name: 'Beginner',
   },
   {
     description:
       'Has a little knowledge and little skill. It is taking its first steps in the theme and is learning.',
+    matrizLevel: 2,
     name: 'Apprentice',
   },
   {
     description:
       'Has a good knowledge of the subject. You are practicing without major problems, getting good results.',
+    matrizLevel: 3,
     name: 'Practitioner',
   },
   {
     description:
       'Has a good knowledge and skill on the subject. It also takes the lead in solving problems.',
+    matrizLevel: 4,
     name: 'Professional',
   },
   {
     description:
       'Master the theme. It is an expert. It stands out for teaching other people. It is a reference for the team.',
+    matrizLevel: 5,
     name: 'Teacher',
   },
   {
     description:
       'He/she can combine knowledge to further improve his/her work, and is a natural leader on this topic.',
+    matrizLevel: 6,
     name: 'Leader',
   },
   {
     description:
       'It creates new knowledge on the subject, totally dominates its execution and is an inspiration to the team.',
+    matrizLevel: 7,
     name: 'Master',
   },
 ];
