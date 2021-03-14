62902d8f3c5ae1a4b16505627d95947ab591fd27
Keven Leone <keven.santos.sz@gmail.com>
2021-03-14 18:11:58 -0300

    #62 Update Lang

--- packages/i18n/pt_BR.ts
+++ packages/i18n/pt_BR.ts
@@ -1,10 +1,13 @@
 export default {
   'add-more-skills': 'Adicione como habilidade',
+  'all-x': 'Todos ({0})',
   'app-title': 'Liferay Grow',
   'app-title-x': 'Grow Together - {0}',
+  'application-submitted-successfully': 'Formulário submetido com sucesso!',
   copyright: 'Copyright 2021. Todos os direitos reservados.',
   'dont-have-a-github-account': 'Não tem uma conta do Github?',
   'get-started': 'Comece agora',
+  'go-to-home': 'Ir para o início',
   'i-feel-comfortable-with-being-a-mentor':
     'Me sinto confortável em ser mentor.',
   'knowledge-areas': ' Áreas de conhecimento',
@@ -21,6 +24,8 @@ export default {
   team: 'Time',
   teams: 'Times',
   'terms-of-use': 'Termos de uso | Políticas de privacidade',
+  'thanks-for-helping-us-to-know-you-better':
+    'Obrigado por nos ajudar, a conhece-lo melhor.',
   'welcome-x': 'Bem vindo, {0}',
   'x-members': '{0} Membros',
 };
