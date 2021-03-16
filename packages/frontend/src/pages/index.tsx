2aff6d918392adf9f000978e11e02443ae37edda
Keven Leone <keven.santos.sz@gmail.com>
2021-03-16 17:36:32 -0300

    #75 Add SEO in Home page

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Meta from '../components/meta';
+import useLang from '../hooks/useLang';
 import Layout from './_template';
 
-const Index = () => {
-  return <Layout>a</Layout>;
+const Index: React.FC = () => {
+  const i18n = useLang();
+
+  return (
+    <Layout>
+      <Meta title={i18n.sub('app-title-x', 'knowledge-areas')} />
+    </Layout>
+  );
 };
 
 export default Index;
