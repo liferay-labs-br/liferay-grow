413eb0c1d74793436db1d8ff6d46ba19d971a42d
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-03-17 01:05:56 -0300

    #78 Config alias to frontend package

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,5 +1,11 @@
-const withImages = require('next-images')
+const path = require('path');
+const withImages = require('next-images');
 
 module.exports = withImages({
     esModules: true,
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    }
 })
