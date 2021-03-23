011a68c971a5319fb2ea558f94e18d92f2a649a4
Keven Leone <keven.santos.sz@gmail.com>
2021-03-23 10:14:27 -0300

    #99 Update GraphQL Query

--- packages/frontend/src/components/templates/TeamTemplate.tsx
+++ packages/frontend/src/components/templates/TeamTemplate.tsx
@@ -2,18 +2,24 @@ import { useRouter } from 'next/router';
 import React from 'react';
 
 import WrappedSafeComponent from '@/components/WrappedSafeComponent';
-import { getTeam } from '@/graphql/queries';
+import { getTeamBySlug } from '@/graphql/queries';
 
 import useLang from '../../hooks/useLang';
 import Header from '../header';
+import Meta from '../meta';
 import NavigationTabBar from '../navigation-tab-bar';
 
 export type Tab = {
   label: string;
   path: string;
+  children: React.ReactNode;
 };
 
-const TeamTemplate: React.FC = ({ children }) => {
+type TeamTeplateProps = {
+  page: string;
+};
+
+const TeamTemplate: React.FC<TeamTeplateProps> = ({ children, page }) => {
   const {
     query: { team },
   } = useRouter();
@@ -29,33 +35,48 @@ const TeamTemplate: React.FC = ({ children }) => {
       path: `/team/${team}/members`,
     },
   ];
-  console.log(team);
+
+  if (!team) {
+    return null;
+  }
+
   return (
-    <WrappedSafeComponent query={getTeam} options={{ variables: { team } }}>
-      {({
-        getTeam: {
+    <WrappedSafeComponent
+      query={getTeamBySlug}
+      options={{ variables: { slug: team } }}
+    >
+      {({ getTeamBySlug }) => {
+        const {
           members,
           name,
           office: { city, country },
-        },
-      }) => {
+        } = getTeamBySlug;
+
         const totalItems = members?.pagination?.totalItems;
+
         return (
-          <div className="team">
-            <div className="team__header">
-              <Header border={false}>
-                <Header.Info>
-                  <Header.Title>{name}</Header.Title>
-                  <p>{`${totalItems || 0} ${
-                    totalItems > 1 ? 'members' : 'member'
-                  }`}</p>
-                  <p>{`${city}, ${country}`}</p>
-                </Header.Info>
-              </Header>
+          <>
+            <Meta title={`${name} - ${i18n.get(page)}`}></Meta>
+            <div className="team">
+              <div className="team__header">
+                <Header border={false}>
+                  <Header.Info>
+                    <Header.Title>{name}</Header.Title>
+                    <p>{`${totalItems || 0} ${
+                      totalItems > 1 ? 'members' : 'member'
+                    }`}</p>
+                    <p>{`${city}, ${country}`}</p>
+                  </Header.Info>
+                </Header>
+              </div>
+              <NavigationTabBar tabs={tabs} />
+              <div className="team__body">
+                {typeof children === 'function'
+                  ? children({ getTeamBySlug })
+                  : children}
+              </div>
             </div>
-            <NavigationTabBar tabs={tabs} />
-            <div className="team__body">{children}</div>
-          </div>
+          </>
         );
       }}
     </WrappedSafeComponent>
