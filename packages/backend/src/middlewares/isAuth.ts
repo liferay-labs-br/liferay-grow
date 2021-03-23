e276d056ad0e0691bd72f73fb71b2c55124b38e7
Keven Leone <keven.santos.sz@gmail.com>
2021-03-23 10:13:39 -0300

    #98 Remove unused code in GraphQL Server

--- packages/backend/src/middlewares/isAuth.ts
+++ packages/backend/src/middlewares/isAuth.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { MiddlewareFn } from 'type-graphql';
 
 import { MyContext } from '../interfaces';
-import { constants, getGraphqlOperation, logger } from '../utils/globalMethods';
+import { constants } from '../utils/globalMethods';
 
 const { AUTH_INVALID_TOKEN, AUTH_NOT_FOUND } = constants;
 const { AUTH_MIDDLEWARE_ENABLED, JWT_SECRET } = process.env;
@@ -10,18 +10,13 @@ const { AUTH_MIDDLEWARE_ENABLED, JWT_SECRET } = process.env;
 export const isAuth: MiddlewareFn<MyContext> = async (ctx, next) => {
   if (AUTH_MIDDLEWARE_ENABLED) {
     const {
-      body,
       headers: { authorization },
     } = ctx.context.req;
-    const operationName = getGraphqlOperation(body.query);
     if (authorization) {
       const token: string = authorization.split(' ').pop() || '';
       try {
         const user: any = jwt.verify(token, JWT_SECRET as string);
         ctx.context.req.headers.loggedUser = user;
-        logger.debug(
-          `${user.name} is running a graphQL request to ${operationName}`,
-        );
         return next();
       } catch (e) {
         throw new Error(AUTH_INVALID_TOKEN);
