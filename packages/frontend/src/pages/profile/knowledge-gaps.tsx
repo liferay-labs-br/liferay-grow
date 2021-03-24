b52499d7cdc8697d6ef2f4576b91f9da897d45ed
Keven Leone <keven.santos.sz@gmail.com>
2021-03-24 15:50:10 -0300

    #107 Create Skill Gaps Page (Edit GrowMap)

---
+++ packages/frontend/src/pages/profile/knowledge-gaps.tsx
@@ -0,0 +1,79 @@
+import { useMutation } from '@apollo/client';
+import React from 'react';
+import { toast } from 'react-toastify';
+
+import { UserSkillTemplate } from '@/components/templates/ProfileSkillTemplate';
+import WrappedSafeComponent from '@/components/WrappedSafeComponent';
+import { UpdateGrowMapGapsDetails } from '@/graphql/mutations';
+import { getMe } from '@/graphql/queries';
+import withAuth from '@/hocs/withAuth';
+import useLang from '@/hooks/useLang';
+import { KnowledgeMatriz, Me, SelectedSkills } from '@/types';
+
+type RequestProps = {
+  allKnowledgeMatriz: KnowledgeMatriz[];
+  me: Me;
+};
+
+const ProfileKnowledgeGaps = () => {
+  const i18n = useLang();
+
+  const [onUpdateGrowMapGaps] = useMutation(UpdateGrowMapGapsDetails);
+
+  const onSave = async (selectedSkills: SelectedSkills[]) => {
+    const knowledgeGapsDetails = selectedSkills.map(({ knowledgeSkillId }) => ({
+      knowledgeSkillId,
+    }));
+
+    try {
+      await onUpdateGrowMapGaps({
+        variables: {
+          data: {
+            knowledgeGapsDetails,
+          },
+        },
+      });
+
+      toast.info(i18n.get('your-request-completed-successfully'));
+    } catch (error) {
+      toast.error(i18n.get('an-unexpected-error-occurred'));
+    }
+  };
+
+  return (
+    <WrappedSafeComponent query={getMe}>
+      {({ me }: RequestProps) => {
+        const knowledgeGapsDetails = me.growMap?.knowledgeGapsDetails || [];
+        const knowledgeSkillDetails = me.growMap?.knowledgeSkillDetails || [];
+
+        const selectedSkills: SelectedSkills[] = knowledgeGapsDetails.map(
+          ({ id, knowledgeSkill }) => ({
+            id,
+            knowledgeSkillId: knowledgeSkill.id,
+          }),
+        );
+
+        const knowledgeSkills: SelectedSkills[] = knowledgeSkillDetails.map(
+          ({ knowledgeSkill }) => ({
+            knowledgeSkillId: knowledgeSkill.id,
+          }),
+        );
+
+        return (
+          <UserSkillTemplate
+            me={me}
+            title={i18n.get('knowledge-gaps')}
+            defaultState={{
+              knowledgeMatrizLevelAllowed: false,
+              selectedSkills: selectedSkills,
+              unavailableKnowledgeSkills: knowledgeSkills,
+            }}
+            onSave={onSave}
+          />
+        );
+      }}
+    </WrappedSafeComponent>
+  );
+};
+
+export default withAuth(ProfileKnowledgeGaps);
