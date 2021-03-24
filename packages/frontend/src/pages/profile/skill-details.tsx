d241aafb986eea5a09f0f93d84002bcc86f81f6d
Keven Leone <keven.santos.sz@gmail.com>
2021-03-24 15:49:58 -0300

    #106 Create Skill Details Page (Edit GrowMap)

---
+++ packages/frontend/src/pages/profile/skill-details.tsx
@@ -0,0 +1,73 @@
+import { useMutation } from '@apollo/client';
+import React from 'react';
+import { toast } from 'react-toastify';
+
+import { UserSkillTemplate } from '@/components/templates/ProfileSkillTemplate';
+import WrappedSafeComponent from '@/components/WrappedSafeComponent';
+import { UpdateGrowMapSkillDetails } from '@/graphql/mutations';
+import { getMe } from '@/graphql/queries';
+import withAuth from '@/hocs/withAuth';
+import useLang from '@/hooks/useLang';
+import { KnowledgeMatriz, Me, SelectedSkills } from '@/types';
+
+type RequestProps = {
+  allKnowledgeMatriz: KnowledgeMatriz[];
+  me: Me;
+};
+
+const ProfileSkillDetails = () => {
+  const i18n = useLang();
+
+  const [onUpdateGrowMapSkills] = useMutation(UpdateGrowMapSkillDetails);
+
+  const onSave = async (selectedSkills: SelectedSkills[]) => {
+    const knowledgeSkillDetails = selectedSkills.map(
+      ({ isMentor, knowledgeMatrizId, knowledgeSkillId }) => ({
+        isMentor,
+        knowledgeMatrizId,
+        knowledgeSkillId,
+      }),
+    );
+
+    try {
+      await onUpdateGrowMapSkills({
+        variables: {
+          data: {
+            knowledgeSkillDetails,
+          },
+        },
+      });
+
+      toast.info(i18n.get('your-request-completed-successfully'));
+    } catch (error) {
+      toast.error(i18n.get('an-unexpected-error-occurred'));
+    }
+  };
+
+  return (
+    <WrappedSafeComponent query={getMe}>
+      {({ me }: RequestProps) => {
+        const knowledgeSkillDetails = me.growMap?.knowledgeSkillDetails || [];
+
+        const selectedSkills: SelectedSkills[] = knowledgeSkillDetails.map(
+          ({ isMentor = false, knowledgeMatriz, knowledgeSkill }) => ({
+            isMentor,
+            knowledgeMatrizId: knowledgeMatriz.id,
+            knowledgeSkillId: knowledgeSkill.id,
+          }),
+        );
+
+        return (
+          <UserSkillTemplate
+            title={i18n.get('skills-details')}
+            me={me}
+            defaultState={{ selectedSkills }}
+            onSave={onSave}
+          />
+        );
+      }}
+    </WrappedSafeComponent>
+  );
+};
+
+export default withAuth(ProfileSkillDetails);
