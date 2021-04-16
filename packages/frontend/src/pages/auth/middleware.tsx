0d1eeaa862ef842331eecf6ebe867e4dbbc42dcf
Keven Leone <keven.santos.sz@gmail.com>
2021-04-16 18:45:55 -0300

    #206 The error should be displayed when the user try to login in the application

--- packages/frontend/src/pages/auth/middleware.tsx
+++ packages/frontend/src/pages/auth/middleware.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
+import ClayButton from '@clayui/button';
 import { useRouter } from 'next/router';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 import AppContext from '@/AppContext';
@@ -18,41 +19,72 @@ const AuthMiddleware: React.FC = () => {
   const router = useRouter();
   const i18n = useLang();
 
+  const docs = {
+    'not-a-liferay-member': {
+      link:
+        'https://github.com/liferay-labs-br/liferay-grow/blob/master/docs/not-a-liferay-member.md',
+      message: i18n.sub('read-more-about-x', 'not-a-liferay-member'),
+    },
+  };
+
   const [onAuthGithub, { loading }] = useMutation(authGithub);
   const { dispatch } = useContext(AppContext);
+  const [error, setError] = useState<string>();
 
-  const fetchUserGithub = async () => {
+  const getUserToken = async () => {
     const urlParams = new URLSearchParams(location.search);
     const code = urlParams.get('code');
 
     if (code) {
-      const {
-        data: { authGithub: bearer },
-      } = await onAuthGithub({ variables: { code } });
+      try {
+        const {
+          data: { authGithub: bearer },
+        } = await onAuthGithub({ variables: { code } });
 
-      dispatch({ payload: { token: bearer }, type: Types.SET_LOGGED_USER });
+        dispatch({ payload: { token: bearer }, type: Types.SET_LOGGED_USER });
 
-      const decodedToken = parseJwt(bearer);
+        const decodedToken = parseJwt(bearer);
 
-      toast.info(i18n.sub('welcome-x', decodedToken?.name));
+        toast.info(i18n.sub('welcome-x', decodedToken?.name));
 
-      router.replace(
-        decodedToken?.user?.growMap ? ROUTES.HOME : ROUTES.WELCOME,
-      );
+        router.replace(
+          decodedToken?.user?.growMap ? ROUTES.HOME : ROUTES.WELCOME,
+        );
+      } catch (err) {
+        toast.error(i18n.get(err.message));
+
+        setError(err.message);
+      }
     } else {
       router.replace(ROUTES.AUTH);
     }
   };
 
   useEffect(() => {
-    fetchUserGithub();
+    getUserToken();
   }, []);
 
   return (
     <AuthTemplate>
       <SEO title={i18n.sub('app-title-x', 'Auth Middleware')} />
 
-      {loading ? (
+      {error ? (
+        <div>
+          {docs[error] ? (
+            <p>
+              <a target="_blank" href={docs[error].link} rel="noreferrer">
+                {docs[error].message}
+              </a>
+            </p>
+          ) : (
+            <p>{i18n.get(error)}</p>
+          )}
+
+          <ClayButton className="mt-4" onClick={() => router.replace('/auth')}>
+            {i18n.get('try-again')}
+          </ClayButton>
+        </div>
+      ) : loading ? (
         <LoadingWrapper />
       ) : (
         <div>{`${i18n.get('redirecting')}...`}</div>
