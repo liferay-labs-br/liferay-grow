3002c4a0994b06cb5ce5b4d2601e7da1b751f867
Keven Leone <keven.santos.sz@gmail.com>
2021-04-19 18:46:58 -0300

    #211 Create a Relationship between UserDetails and Department

--- packages/backend/src/entity/UserDetails.ts
+++ packages/backend/src/entity/UserDetails.ts
@@ -1,6 +1,7 @@
 import { Field, ObjectType } from 'type-graphql';
 import { Entity, JoinColumn, JoinTable, ManyToMany, OneToOne } from 'typeorm';
 
+import { Department } from './Department';
 import { MainEntity } from './MainEntity';
 import { Office } from './Office';
 import { Role } from './Role';
@@ -10,14 +11,19 @@ import { Team } from './Team';
 @Entity()
 export class UserDetails extends MainEntity {
   @JoinColumn()
-  @Field(() => Role, { nullable: true })
-  @OneToOne(() => Role, { createForeignKeyConstraints: false })
-  role?: Role;
+  @Field(() => Department, { nullable: true })
+  @OneToOne(() => Department, { createForeignKeyConstraints: false })
+  department?: Department;
 
   @JoinColumn()
   @Field(() => Office, { nullable: true })
   @OneToOne(() => Office, { createForeignKeyConstraints: false })
   office?: Office;
+
+  @JoinColumn()
+  @Field(() => Role, { nullable: true })
+  @OneToOne(() => Role, { createForeignKeyConstraints: false })
+  role?: Role;
 
   @Field(() => [Team], { nullable: true })
   @JoinTable({ name: 'user_details_teams' })
