56989a9a65c2897fae4c2d9796d2dd3706723317
Keven Leone <keven.santos.sz@gmail.com>
2021-04-20 18:26:12 -0300

    #218 Use Directly download calls, in backend and frontend

--- packages/backend/src/controller/CSV.ts
+++ packages/backend/src/controller/CSV.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs';
 
 export class CSVController {
+  protected tempFolder = 'temp';
+
   protected async getFileWS(filename: string): Promise<fs.WriteStream> {
-    if (!fs.existsSync('temp')) {
-      fs.mkdirSync('temp');
+    if (!fs.existsSync(this.tempFolder)) {
+      fs.mkdirSync(this.tempFolder);
     }
-    return fs.createWriteStream(`temp/${filename}`);
+    return fs.createWriteStream(`${this.tempFolder}/${filename}`);
   }
 }
