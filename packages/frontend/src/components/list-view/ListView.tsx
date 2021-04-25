77564a3a25244b46195271a926d142862340cc80
Keven Leone <keven.santos.sz@gmail.com>
2021-04-25 20:19:33 -0300

    #227 Rewrite ListView in new props

--- packages/frontend/src/components/list-view/ListView.tsx
+++ packages/frontend/src/components/list-view/ListView.tsx
@@ -10,56 +10,114 @@ import TableComponent from '../table';
 
 const deltas = [10, 20, 40, 50, 100].map((delta) => ({ label: delta }));
 
+type FilterItems = {
+  label: string;
+  name: string;
+  active?: boolean;
+  onClick?: () => void;
+};
+
+type Variables = {
+  find?: any;
+  order?: string;
+  pageIndex?: number;
+  pageSize?: number;
+  sort?: string;
+};
+
 type IListView = {
   pagination: Pagination;
   columns: any[];
   items: any[];
+  formatVariables?: (variables: { data: Variables }) => void;
   orderBy?: boolean;
+  filterItems?: FilterItems[];
+  className?: string;
   emptyState?: EmptyStateProps;
+  variables?: {
+    data: Variables;
+  };
+  refetch: (variables: any) => void;
   onPageChange?: (page: number) => void;
   onDeltaChange?: (delta: number) => void;
   searchOnChange?: (value: string) => void;
 };
 
 const ListView: React.FC<IListView> = ({
+  className,
   columns,
   emptyState,
+  filterItems = [],
   items,
-  onDeltaChange = () => {},
-  onPageChange = () => {},
   orderBy,
+  formatVariables,
   pagination,
-  searchOnChange,
+  refetch,
+  variables = {},
 }) => {
   const { currentPage, totalItems = 0 } = pagination || {};
 
-  if (totalItems) {
-    return (
-      <>
-        {searchOnChange && (
-          <ManagementToolbar
-            orderBy={orderBy}
-            searchOnChange={searchOnChange}
+  const doGraphQLOperations = !!refetch;
+
+  const activeSortBy = filterItems.find(({ active }) => active)?.name || 'name';
+
+  const onResourceChange = async (type, value) => {
+    if (doGraphQLOperations) {
+      const data = {
+        data: {
+          ...variables.data,
+          [type]: value,
+        },
+      };
+
+      const formattedVariables = formatVariables ? formatVariables(data) : data;
+
+      await refetch(formattedVariables);
+    }
+  };
+
+  return (
+    <div className={className}>
+      <ManagementToolbar
+        orderBy={orderBy}
+        totalItems={pagination.totalItems}
+        searchText={
+          variables?.data?.find ? variables.data.find[activeSortBy] : null
+        }
+        onClickSearch={(value) => {
+          onResourceChange('find', {
+            [activeSortBy]: `%${value}%`,
+          });
+        }}
+        onClickOrderBy={() =>
+          onResourceChange(
+            'sort',
+            variables.data.sort === 'ASC' ? 'DESC' : 'ASC',
+          )
+        }
+        filterItems={filterItems}
+      />
+
+      {pagination.totalItems > 0 ? (
+        <>
+          <TableComponent columns={columns} items={items} />
+
+          <ClayPaginationBarWithBasicItems
+            activeDelta={pagination.pageSize}
+            deltas={deltas}
+            activePage={currentPage}
+            spritemap={null}
+            ellipsisBuffer={3}
+            onPageChange={(value) => onResourceChange('pageIndex', value)}
+            onDeltaChange={(value) => onResourceChange('pageSize', value)}
+            totalItems={totalItems}
           />
-        )}
-
-        <TableComponent columns={columns} items={items} />
-
-        <ClayPaginationBarWithBasicItems
-          activeDelta={pagination.pageSize}
-          deltas={deltas}
-          activePage={currentPage}
-          spritemap={null}
-          ellipsisBuffer={3}
-          onPageChange={onPageChange}
-          onDeltaChange={onDeltaChange}
-          totalItems={totalItems}
-        />
-      </>
-    );
-  }
-
-  return <EmptyState {...emptyState} />;
+        </>
+      ) : (
+        <EmptyState {...emptyState} />
+      )}
+    </div>
+  );
 };
 
 export default ListView;
